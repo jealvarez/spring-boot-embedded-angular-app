92f67a34ffb9566c1706d74da40ea190fa21ca63
Jorge Alvarez <alvarez.jeap@gmail.com>
2019-04-25 17:07:56 -0600

    Remove out folder

--- out/production/resources/db/migration/V1__Initial_Migration.sql
+++
@@ -1,8 +0,0 @@
--- -----------------------------------------------------
--- Table example.authority_group
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS example.authority_group (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(45) NOT NULL,
-  PRIMARY KEY (id))
-ENGINE = InnoDB;
